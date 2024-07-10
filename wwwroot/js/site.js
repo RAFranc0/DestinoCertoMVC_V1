61f2c2a7dbdc2626be0b37789018efc7a3d772f2
Raul <raul.franco266@gmail.com>
2024-07-10 01:03:22 -0300

    Tela de usuário concluida

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Scripts para as telas do menu do usuário
+function hideAllExcept(current) {
+    $("#usuario-menu-tela-adicionar, #usuario-menu-tela-editar, #usuario-menu-tela-listar, #usuario-menu-tela-excluir").not(current).hide();
+}
 
-// Write your JavaScript code.
+$("#usuario-menu-btn-adicionar").click(function() {
+    hideAllExcept("#usuario-menu-tela-adicionar");
+    $("#usuario-menu-tela-adicionar").fadeToggle(500);
+});
+
+$("#usuario-menu-btn-editar").click(function() {
+    hideAllExcept("#usuario-menu-tela-editar");
+    $("#usuario-menu-tela-editar").fadeToggle(500);
+});
+
+$("#usuario-menu-btn-listar").click(function() {
+    hideAllExcept("#usuario-menu-tela-listar");
+    $("#usuario-menu-tela-listar").fadeToggle(500);
+});
+
+$("#usuario-menu-btn-excluir").click(function() {
+    hideAllExcept("#usuario-menu-tela-excluir");
+    $("#usuario-menu-tela-excluir").fadeToggle(500);
+});
+
+//Script para a tela de edição
+
+$(document).ready(function() {
+    $('#form-usuario-editar-btn').click(function() {
+        $('#form-usuario-editar').submit();
+    });
+});
